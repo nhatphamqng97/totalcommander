8a701dfd7baf5fd437ce78f0a31aa54846ef8a42
nhatphamqng97 <nhatphamqng@gmail.com>
2018-12-08 12:47:17 +0700

    Load TreeView

--- TotalCommander/Form1.cs
+++ TotalCommander/Form1.cs
@@ -36,8 +36,110 @@ namespace TotalCommander
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            
+            this.gui1 = new GUI.uc_DirectoryList(contextMenu);
+            gui1.Dock = DockStyle.Fill;
+            gui1.getCopyAction = new GUI.uc_DirectoryList.DgetCopyAction(copyAction);
+            gui1.getCopyPath = new GUI.uc_DirectoryList.DgetCopyPath(pushCopyPath);
+            gui1.getPasteAction = new GUI.uc_DirectoryList.DgetPasteAction(pasteAction);
+            gui1.getRefreshAll = new GUI.uc_DirectoryList.DgetRefreshAll(refreshAll);
+            gui1.getContextMenu = new GUI.uc_DirectoryList.DgetContextMenu(pushContextMenu);
+            gui1.pushContextMenuForParent = new GUI.uc_DirectoryList.DpushContextMenuForParent(setContextMenu);
+            gui1.setEnabledButton = new GUI.uc_DirectoryList.DsetEnabledButton(setEnabledForSelectItem);
+            splitMain.Panel1.Controls.Add(gui1);
+            //------------------------------------------------------------------------------------
+            this.gui2 = new GUI.uc_DirectoryList(contextMenu);
+            gui2.Dock = DockStyle.Fill;
+            gui2.getCopyAction = new GUI.uc_DirectoryList.DgetCopyAction(copyAction);
+            gui2.getCopyPath = new GUI.uc_DirectoryList.DgetCopyPath(pushCopyPath);
+            gui2.getPasteAction = new GUI.uc_DirectoryList.DgetPasteAction(pasteAction);
+            gui2.getRefreshAll = new GUI.uc_DirectoryList.DgetRefreshAll(refreshAll);
+
+            gui2.getContextMenu = new GUI.uc_DirectoryList.DgetContextMenu(pushContextMenu);
+            gui2.pushContextMenuForParent = new GUI.uc_DirectoryList.DpushContextMenuForParent(setContextMenu);
+            gui2.setEnabledButton = new GUI.uc_DirectoryList.DsetEnabledButton(setEnabledForSelectItem);
+            splitMain.Panel2.Controls.Add(gui2);
         }
+
+        public void refreshAll()
+        {
+            this.gui1.showDirectoryAndFiles(this.gui1.ListBack.Peek());
+            this.gui2.showDirectoryAndFiles(this.gui2.ListBack.Peek());
+        }
+
+        public void setEnabledForSelectItem(bool isSelected)
+        {
+            btnCopy.Enabled = btnCut.Enabled = btnDelete.Enabled = btnRename.Enabled = isSelected;
+        }
+
+        #region ContextMenu
+
+        private void setContextMenu()//Thay đổi giá trị item của contextMenu tương ứng với các item bên ngoài
+        {
+            this.contextMenu.Items[0].Enabled = menuItemOpen.Enabled;
+            this.contextMenu.Items[1].Enabled = menuItemCopy.Enabled;
+            this.contextMenu.Items[2].Enabled = menuItemCut.Enabled;
+            this.contextMenu.Items[3].Enabled = menuItemPaste.Enabled;
+        }
+
+        //Đẩy contextMenu cho giao diện con
+        public ContextMenuStrip pushContextMenu()
+        {
+            setContextMenu();
+            return this.contextMenu;
+        }
+
+        //Lấy contextMenu từ giao diện con
+        public void setContextMenu(ContextMenuStrip contextMenu)
+        {
+            this.contextMenu = contextMenu;
+        }
+
+        #endregion
+
+        #region Thao Tác với file
+
+        private List<string> listCopyPath { get; set; }
+
+        private bool isCopy { get; set; }
+
+        private void copyAction(List<string> listPath, bool isCopy)
+        {
+            this.isCopy = isCopy;
+
+            this.listCopyPath.Clear();
+
+            this.listCopyPath = listPath;
+        }
+
+        //Đẩy đường dẫn cần copy(ListCopyPath) cho giao diện con
+        public List<string> pushCopyPath()
+        {
+            return this.listCopyPath;
+        }
+
+        //Xử lý khi người dùng paste
+        public void pasteAction(string pastePath)
+        {
+            if (isCopy)
+            {
+                foreach (string path in this.listCopyPath)
+                    if (!BLL.ClassBLL.Instances.copyAction(path, pastePath))
+                        return;
+            }
+            else
+            {
+                foreach (string path in this.listCopyPath)
+                    if (!BLL.ClassBLL.Instances.moveAction(path, pastePath))
+                        return;
+
+                this.isCopy = true;
+
+                this.listCopyPath.Clear();
+            }
+        }
+
+        #endregion
+
 
         private void btnPack_ItemClick(object sender, ItemClickEventArgs e)
         {
